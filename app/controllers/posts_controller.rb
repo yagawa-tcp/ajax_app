9184a2594110255b013557f289d6fc09f1bb1639
yagawa-tcp <69143961+yagawa-tcp@users.noreply.github.com>
2020-08-17 17:05:13 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all.orde(id: "DESC")
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
@@ -9,4 +9,15 @@ class PostsController < ApplicationController
       redirect_to action: :index
   end
 
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
